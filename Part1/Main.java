6785cd1df09ab5c73b914248a8d7567785ff3480
Tatiana Klimenko <vanshooky0.0@gmail.com>
2022-10-06 18:54:41 +0300

    x _ x

--- Part1/Main.java
+++ Part1/Main.java
@@ -39,54 +39,54 @@ public class Main {
 
     public static void main(String[] args) {
         
-                //     Double[][] matrixA = {
-                //         {2.5, -3.0, 4.6}, 
-                //         {-3.5, 2.6, 1.5,}, 
-                //         {-6.5, -3.5, 7.3,} 
-                //     };
-                //     Double[][] matrixA1 = {
-                //         {2.5, -3.0, 4.6}, 
-                //         {-3.5, 2.6, 1.5,}, 
-                //         {-6.5, -3.5, 7.3,} 
-                //     };
-                //     Double[][] matrixA2 = {
-                //         {2.5, -3.0, 4.6}, 
-                //         {-3.5, 2.6, 1.5,}, 
-                //         {-6.5, -3.5, 7.3,} 
-                //     };
-                //     Double[] matrixB = {-1.05, -14.46, -17.73};
-                //     Double[] matrixB1 = {-1.05, -14.46, -17.73};
+                    // Double[][] matrixA = {
+                    //     {2.5, -3.0, 4.6}, 
+                    //     {-3.5, 2.6, 1.5,}, 
+                    //     {-6.5, -3.5, 7.3,} 
+                    // };
+                    // Double[][] matrixA1 = {
+                    //     {2.5, -3.0, 4.6}, 
+                    //     {-3.5, 2.6, 1.5,}, 
+                    //     {-6.5, -3.5, 7.3,} 
+                    // };
+                    // Double[][] matrixA2 = {
+                    //     {2.5, -3.0, 4.6}, 
+                    //     {-3.5, 2.6, 1.5,}, 
+                    //     {-6.5, -3.5, 7.3,} 
+                    // };
+                    // Double[] matrixB = {-1.05, -14.46, -17.73};
+                    // Double[] matrixB1 = {-1.05, -14.46, -17.73};
                     
-                //     Double[] matrixX = SLAE.GaussMethod(matrixA, matrixB);
-                //     System.out.println("Ответ:");
-                //     for (int i = 0; i < matrixX.length; i++) {
-                //         System.out.print("x" + ++i + " = " + matrixX[--i] + "  ");
-                //         System.out.println();
-                //     }
+                    // Double[] matrixX = SLAE.GaussMethod(matrixA, matrixB);
+                    // System.out.println("Ответ:");
+                    // for (int i = 0; i < matrixX.length; i++) {
+                    //     System.out.print("x" + ++i + " = " + matrixX[--i] + "  ");
+                    //     System.out.println();
+                    // }
                     
-                //     Double[] residualVector = SLAE.residualVector(matrixA1, matrixB1, matrixX);
-                //     System.out.print("Вектор невязки: ");
-                //     System.out.println(Arrays.toString(residualVector));
-                //     System.out.println("Норма: " + SLAE.getNorm(residualVector));
-                //     System.out.println();
-                //     SLAE.RelativeError(matrixA2, matrixX);
-                //     System.out.println("Относительная погрешность: " + SLAE.RelativeError(matrixA2, matrixX));
-                //     System.out.println();
+                    // Double[] residualVector = SLAE.residualVector(matrixA1, matrixB1, matrixX);
+                    // System.out.print("Вектор невязки: ");
+                    // System.out.println(Arrays.toString(residualVector));
+                    // System.out.println("Норма: " + SLAE.getNorm(residualVector));
+                    // System.out.println();
+                    // SLAE.RelativeError(matrixA2, matrixX);
+                    // System.out.println("Относительная погрешность: " + SLAE.RelativeError(matrixA2, matrixX));
+                    // System.out.println();
 
-                //     double l1 = 1, l2 = 10, l3 = 15;
-                //     Double[][] matrixALDL = {
-                //         {2 * l1 + 4 * l2, 2 * (l1 - l2), 2 * (l1 - l2)}, 
-                //         {2 * (l1 - l2), 2 * l1 + l2 + 3 * l3, 2 * l1 + l2 - 3 * l3}, 
-                //         {2 * (l1 - l2), 2 * l1 + l2 - 3 * l3, 2 * l1 + l2 + 3 * l3}
-                //     };
-                //     Double[] matrixBLDL = {-4 * l1 - 2 * l2, -4 * l1 + l2 + 9 * l3, -4 * l1 + l2 - 9 * l3};
-                //     if (SLAE.isSymmetric(matrixALDL)) {
-                //         Double[] matrixXLDL = SLAE.FactorizationMethod(matrixALDL, matrixBLDL);
-                //         System.out.print("Ответ: ");
-                //         System.out.println(Arrays.toString(matrixXLDL));
-                //     } else {
-                //         throw new ArithmeticException("Матрица не симметричная.");
-                //     }
+                    // double l1 = 1, l2 = 10, l3 = 15;
+                    // Double[][] matrixALDL = {
+                    //     {2 * l1 + 4 * l2, 2 * (l1 - l2), 2 * (l1 - l2)}, 
+                    //     {2 * (l1 - l2), 2 * l1 + l2 + 3 * l3, 2 * l1 + l2 - 3 * l3}, 
+                    //     {2 * (l1 - l2), 2 * l1 + l2 - 3 * l3, 2 * l1 + l2 + 3 * l3}
+                    // };
+                    // Double[] matrixBLDL = {-4 * l1 - 2 * l2, -4 * l1 + l2 + 9 * l3, -4 * l1 + l2 - 9 * l3};
+                    // if (SLAE.isSymmetric(matrixALDL)) {
+                    //     Double[] matrixXLDL = SLAE.FactorizationMethod(matrixALDL, matrixBLDL);
+                    //     System.out.print("Ответ: ");
+                    //     System.out.println(Arrays.toString(matrixXLDL));
+                    // } else {
+                    //     throw new ArithmeticException("Матрица не симметричная.");
+                    // }
 
                 Double[] init = {1.0, 1.0};
                 Function<Double[], Double>[] functions = new Function[init.length];
