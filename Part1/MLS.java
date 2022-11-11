5d29b3580391813e4c4043f5f392842fb8a23576
Tatiana Klimenko <vanshooky0.0@gmail.com>
2022-11-11 23:21:47 +0300

    вроде вывод нужен красивый, но не судьба

--- Part1/MLS.java
+++ Part1/MLS.java
@@ -5,7 +5,7 @@ import static java.lang.Math.*;
 public class MLS {
 
     public static final int N = 10;
-    public static final int m = 2;
+    public static final int m = 1;
     public static final Double[] data_x = {1.2, 1.82, 3.31, 7.24, 8.92, 9.12, 10.97, 14.45, 22.91, 36.31};
     public static final Double[] data_y = {630.0, 890.0, 2350.0, 3810.0, 4630.0, 4820.0, 5230.0, 7500.0, 11800.0, 19600.0};
 
@@ -17,12 +17,19 @@ public class MLS {
         Double[][] sumX = formSumX(powerX);
         Double[] praw = formPraw();
         Double[] coefficients = SLAE.gaussMethod(sumX, praw);
-
+        System.out.println("coefficients:" + Arrays.toString(coefficients));
         Double standardDeviation = standardDeviation(coefficients);
         System.out.println("\ndeviation:" + standardDeviation);
-        Double[] approximationFunc = approximationFunc();
-        System.out.println(Arrays.toString(approximationFunc));
+        Double[] aproximationFunc = aproximationFunc(coefficients);
+        System.out.println(Arrays.toString(aproximationFunc));
+    }
 
+    public static Double[] aproximationFunc(Double[] coefficient){
+        Double[] funk = new Double[N];
+        for(int i = 0; i < N; i++){
+            funk[i] = coefficient[0] + coefficient[1] * data_x[i];
+        }
+        return funk;
     }
 
     public static Double[] powerX() {
@@ -73,64 +80,11 @@ public class MLS {
             S += temp * temp;
         }
         S /= (N * m - 1);
-                // System.out.println("S=" + S);
         return S;
     }
     
     public static Double standardDeviation( Double[] coefficients) {
         Double standardDeviation = sqrt(dispersion(coefficients));
         return standardDeviation;
-    }
-
-    public static Double[] approximationFunc(){
-        Double[] func = new Double[N];
-        Double a = calculate_a();
-        Double b = calculate_b();
-        System.out.println("\na = " + a + " b = " + b);
-
-        for(int i = 0; i < N; i++){
-            func[i] = a * data_x[i] + b;
-        }
-        return func;
-    }
-
-    public static Double calculate_a(){
-        Double a;
-        final Double averageXY = average(data_x, data_y);
-        final Double averageX = average(data_x);
-        final Double averageY = average(data_y);
-        final Double averageXX = average(data_x, data_x);
-
-        a = (averageXY - averageX * averageY)/(averageXX - averageX * averageX);
-        return a;
-    }
-
-    public static Double calculate_b(){
-        Double b;
-        final Double averageXY = average(data_x, data_y);
-        final Double averageX = average(data_x);
-        final Double averageY = average(data_y);
-        final Double averageXX = average(data_x, data_x);
-
-        b = (averageY * averageXX - averageX * averageXY)/(averageXX - averageX * averageX);
-        return b;
-    }
-
-    public static Double average(Double[] value){
-        Double average = 0.0;
-        for(int i = 0; i < N; i++){
-            average += value[i];
-        }
-        average /= N;
-        return average;
-    }
-
-    public static Double average(Double[] value1, Double[] value2){
-        Double average = 0.0;
-        for(int i = 0; i < N; i++){
-            average += value1[i] * value2[i];
-        }
-        average /= N;
-        return average;
     }
 }
